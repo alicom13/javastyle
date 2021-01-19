b83c3fdf44070b07ad99362cc33d337c05b78499
Ali Musthofa <alfaonline13@gmail.com>
2021-01-19 15:30:53 +0700

    Create Hassclass.js

---
+++ Hassclass.js
@@ -0,0 +1,11 @@
+if($(".small-paragraph").hasClass("pretty")){
+   $(".small-paragraph").removeClass("pretty");}
+else {
+   $(".small-paragraph").addClass("pretty"); }
+
+if($("div").hasClass("btn")){
+   $("div").removeClass("btn");
+   $("div").addClass("btn-sm");
+}
+else {
+   $("button").addClass("btn-sm"); }
